5b69b3baec25b30645edd7ca9ef4409030434cd1
Greg <no@no.com>
2025-04-11 11:23:01 -0400

    finished math calc

--- workbooks/workbook-1/math_calculator/src/main/java/com/ps/Main.java
+++ workbooks/workbook-1/math_calculator/src/main/java/com/ps/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("1) Addition");
@@ -19,7 +20,46 @@ public class Main {
         System.out.print("Second whole number: ");
         float secondNum = scanner.nextFloat();
 
+        float result = 0;
 
+        switch(givenCommand){
+            case 1:
+                result = firstNum + secondNum;
+                break;
+            case 2:
+                result = firstNum - secondNum;
+                break;
+            case 3:
+                result = firstNum * secondNum;
+                break;
+            case 4:
+                if(secondNum == 0){
+                    System.out.println("You can't divide by zero");
+                } else {
+                    result = firstNum / secondNum;
+                }
+                break;
+            default:
+                System.out.println("Invalid command, try again");
+        }
+
+//        if(givenCommand == 1){
+//            result = firstNum + secondNum;
+//        } else if(givenCommand == 2) {
+//            result = firstNum - secondNum;
+//        } else if(givenCommand == 3){
+//            result = firstNum * secondNum;
+//        } else if(givenCommand == 4){
+//            if(secondNum == 0){
+//                System.out.println("You can't divide by zero");
+//            } else {
+//                result = firstNum / secondNum;
+//            }
+//        } else {
+//            System.out.println("Invalid command, try again");
+//        }
+
+        System.out.printf("Result: %.3f", result);
 
     }
 }
