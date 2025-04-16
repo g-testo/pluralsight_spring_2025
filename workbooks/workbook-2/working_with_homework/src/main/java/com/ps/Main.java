91a910391acf25cbe796d160f5788d5eddc6be8c
Greg <no@no.com>
2025-04-16 11:37:12 -0400

    finshed homework review

--- workbooks/workbook-2/working_with_homework/src/main/java/com/ps/Main.java
+++ workbooks/workbook-2/working_with_homework/src/main/java/com/ps/Main.java
@@ -1,11 +1,41 @@
 package com.ps;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+//        homeworkOne();
+        homeworkTwo();
+
+
+    }
+
+    public static void homeworkTwo(){
+        String teamScoresStr = "Home:Visitor|21:9";
+        String[] teamScoresArr = teamScoresStr.split("\\|"); // [ "Home:Visitor", "21:9" ]
+
+        String teamNames = teamScoresArr[0]; // "Home:Visitor"
+        String scores = teamScoresArr[1]; // "21:9"
+
+        String[] teamNamesArr = teamNames.split(":");// [ "Home", "Visitor" ]
+        String[] scoresArr = scores.split(":"); // [ "21", "9" ] // Compare numbers
+
+        int teamOneScore = Integer.parseInt(scoresArr[0]); // 21
+        int teamTwoScore = Integer.parseInt(scoresArr[1]); // 9
+
+        if(teamOneScore > teamTwoScore){
+            System.out.println("Winner: " + teamNamesArr[0]); // "Winner: Home"
+        } else if(teamTwoScore > teamOneScore){
+            System.out.println("Winner: " + teamNamesArr[1]); // "Winner: Visitor"
+        } else {
+            System.out.println("Tie");
+        }
+    }
+
+    public static void homeworkOne(){
         System.out.println("Please enter your name");
 
         System.out.print("First name: ");
@@ -23,7 +53,10 @@ public class Main {
         String fullName = firstName;
 
         if(!middleName.isEmpty()){
-            fullName += " " + middleName;
+            char firstChar = Character.toUpperCase(middleName.charAt(0));
+//            String firstLetter = Character.toString(middleName.charAt(0));
+//            char firstUpperCaseLetter = firstLetter.toUpperCase();
+            fullName += " " + firstChar + ".";
         }
 
         fullName += " " + lastName;
@@ -33,5 +66,7 @@ public class Main {
         }
 
         System.out.printf("Full name: %s", fullName);
+
     }
+
 }
