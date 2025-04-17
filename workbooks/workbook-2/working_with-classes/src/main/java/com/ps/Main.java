c0d6c222e2752bfc78fe4701ff517898594f1d0e
Greg <no@no.com>
2025-04-17 09:02:53 -0400

    added String builder homework

---
+++ workbooks/workbook-2/working_with-classes/src/main/java/com/ps/Main.java
@@ -0,0 +1,18 @@
+package com.ps;
+
+public class Main {
+    public static void main(String[] args) {
+        String phrase1 = "Early bird gets the worm";
+        String phrase2 = "Measure twice cut once";
+        String phrase3 = "Lefty loosie righty tighty";
+        // Methods
+        // generates a random phrase
+        // display the phrase
+
+
+        String animal1 = "Monkey";
+        String animal2 = "Goat";
+        String animal3 = "Lion";
+        //
+    }
+}
--- workbooks/workbook-2/working_with_stringBuilder/src/main/java/com/ps/Main.java
+++ workbooks/workbook-2/working_with_stringBuilder/src/main/java/com/ps/Main.java
@@ -1,6 +1,10 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 //        StringBuilder myAnimals = new StringBuilder();
 //        myAnimals.append("Lion, ");
@@ -19,14 +23,63 @@ public class Main {
 //        animals+="Monkey";
 //        animals+="KittyCat";
 
-        String result1 = disemvowel("This website is not fun LOL!"); // Ths wbst s fr lsrs LL!
-        String result2 = disemvowel("No offense but,\nYour writing is not good"); // N ffns bt,\nYr wrtng s mng th wrst 'v vr rd
-
-
-        System.out.println(result1);
-        System.out.println(result2);
-
+//        String result1 = disemvowel("This website is not fun LOL!"); // Ths wbst s fr lsrs LL!
+//        String result2 = disemvowel("No offense but,\nYour writing is not good"); // N ffns bt,\nYr wrtng s mng th wrst 'v vr rd
+//
+//
+//        System.out.println(result1);
+//        System.out.println(result2);
+        stringBuilderHomework();
     }
+
+    public static void stringBuilderHomework(){
+        StringBuilder customerDetails = new StringBuilder();
+
+        System.out.println("Please provide the following information:");
+
+        System.out.print("Full name: ");
+        String fullName = scanner.nextLine();
+
+        System.out.print("Billing Street: ");
+        String billingStreet = scanner.nextLine();
+
+        System.out.print("Billing City: ");
+        String billingCity = scanner.nextLine();
+
+        System.out.print("Billing State: ");
+        String billingState = scanner.nextLine();
+
+        System.out.print("Billing Zip: ");
+        String billingZip = scanner.nextLine();
+
+//        System.out.print("Shipping Street: ");
+//        System.out.print("Shipping City:" );
+//        System.out.print("Shipping State:");
+//        System.out.print("Shipping Zip:" );
+
+        customerDetails.append(fullName).append("\n");
+
+        customerDetails.append("Billing Address:\n");
+
+        customerDetails.append(billingStreet).append("\n");
+        customerDetails.append(billingCity);
+        customerDetails.append(", ");
+        customerDetails.append(billingState);
+        customerDetails.append(" ");
+        customerDetails.append(billingZip).append("\n");
+
+        String customDetailsStr = customerDetails.toString();
+        System.out.println(customDetailsStr);
+
+//        Grover Smith
+//        Billing Address:
+//        123 Main Street
+//        Middleton, TX 75111
+//        Shipping Address:
+//        456 Big Sky Blvd
+//        Outer Rim, TX 75333
+    }
+
 
     public static String disemvowel(String str) {
         StringBuilder comment = new StringBuilder();
