5e1ee6ee4ff98b6c5a0eccade8c36d2360010638
Greg <no@no.com>
2025-06-10 10:42:51 -0400

    added drivermanager example

---
+++ workbooks/workbook-8/working_with_jdbc/src/main/java/com/ps/DriverManagerExample.java
@@ -0,0 +1,51 @@
+package com.ps;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class DriverManagerExample {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.print("Please provide a film id: ");
+        String filmId = scanner.nextLine();
+
+        String query = "SELECT * FROM film WHERE film_id = ?";
+
+        try(
+// BAD
+//            Statement statement = connection.createStatement();
+//            String query = "SELECT * FROM film WHERE film_id = " + filmId + ";";
+//            ResultSet resultSet = statement.executeQuery(query);
+// /BAD
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/sakila",
+                        args[0],
+                        args[1]
+                );
+                PreparedStatement statement = connection.prepareStatement(query);
+
+        ) {
+
+
+
+// GOOD
+            statement.setString(1,filmId);
+            ResultSet resultSet = statement.executeQuery();
+// /GOOD
+            while(resultSet.next()){
+                String name = resultSet.getString("title");
+                System.out.println(name);
+            }
+
+            connection.close();
+            statement.close();
+            resultSet.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
--- workbooks/workbook-8/working_with_jdbc/src/main/java/com/ps/Main.java
+++ workbooks/workbook-8/working_with_jdbc/src/main/java/com/ps/Main.java
@@ -1,34 +1,5 @@
 package com.ps;
 
-import java.sql.*;
-
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3307/sakila",
-                    "root",
-                    "password"
-            );
-            Statement statement = connection.createStatement();
 
-            String query = "SELECT * FROM film;";
-
-            ResultSet resultSet = statement.executeQuery(query);
-
-            while(resultSet.next()){
-                String name = resultSet.getString("title");
-                System.out.println(name);
-            }
-
-            connection.close();
-            statement.close();
-            resultSet.close();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-
-    }
 }
