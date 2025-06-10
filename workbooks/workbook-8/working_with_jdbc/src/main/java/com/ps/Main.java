c30db8f3a24c73eea60cbf5d6d2eac1d841a46c3
Greg <no@no.com>
2025-06-10 12:07:49 -0400

    added try with resources

--- workbooks/workbook-8/working_with_jdbc/src/main/java/com/ps/Main.java
+++ workbooks/workbook-8/working_with_jdbc/src/main/java/com/ps/Main.java
@@ -3,6 +3,10 @@ package com.ps;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Arrays;
 
 public class Main {
@@ -10,7 +14,7 @@ public class Main {
         // Add your dependencies to the POM (2!!!) Mysql driver(Core) and dbcp(Data sources)
         // Config runner(Current File) for application with database username and password
 
-        //
+        // Added db login validation
         if(args.length < 2){
             System.out.println("Username and password are required to run. Exiting...");
             System.exit(1);
@@ -18,12 +22,26 @@ public class Main {
         String dbUsername = args[0];
         String dbPassword = args[1];
 
+        // Created and setup the connection to our database
         BasicDataSource dataSource = new BasicDataSource();
 
         dataSource.setUrl("jdbc:mysql://localhost:3306/world");
         dataSource.setUsername(dbUsername);
         dataSource.setPassword(dbPassword);
 
-
+        // Try with resources
+        String query = "SELECT name FROM country;";
+        try(
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            while(resultSet.next()){
+                String name = resultSet.getString("name");
+                System.out.println(name);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
