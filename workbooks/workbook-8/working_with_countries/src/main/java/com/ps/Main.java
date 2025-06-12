9431e4d52e0a3413a7c9966674ebeae10b5969a4
Greg <no@no.com>
2025-06-12 11:31:27 -0400

    added menus

--- workbooks/workbook-8/working_with_countries/src/main/java/com/ps/Main.java
+++ workbooks/workbook-8/working_with_countries/src/main/java/com/ps/Main.java
@@ -4,8 +4,10 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
         if(args.length < 2){
@@ -19,22 +21,77 @@ public class Main {
         basicDataSource.setPassword(args[1]);
         CountryDAO countryDAO = new CountryDAO(basicDataSource);
 
+        int mainCommand;
 
-//        List<Country> countries = countryDAO.getAll();
-//        System.out.println(countries);
+        do{
+            System.out.println("What do you want to do?");
+            System.out.println("1) Get All");
+            System.out.println("2) Get By Code");
+            System.out.println("3) Create country");
+            System.out.println("4) Update country");
+            System.out.println("5) Delete country");
+            System.out.println("0) Exit");
+            System.out.print("Option: ");
+            mainCommand = scanner.nextInt();
 
+            switch (mainCommand){
+                case 1:
+                    List<Country> countries = countryDAO.getAll();
+                    System.out.println(countries);
+                    break;
+                case 2:
+                    scanner.nextLine();
+                    System.out.print("Please provide a country code: ");
+                    String code = scanner.nextLine();
 
-//        Country country = countryDAO.getByCode("UME");
-//        System.out.println(country);
+                    if(code.length() > 3){
+                        System.out.println("Code too long");
+                        break;
+                    }
 
-//        Country country = new Country("UME", "Ummetopia", 1);
-//        countryDAO.create(country);
+                    Country country = countryDAO.getByCode(code);
+                    System.out.println(country);
+                    break;
+                case 3:
+                    System.out.println("Please provide the following");
+                    System.out.print("Code: ");
+                    String countryCode = scanner.nextLine();
+                    System.out.print("Name: ");
+                    String countryName = scanner.nextLine();
+                    System.out.print("Population: ");
+                    long countryPopulation = scanner.nextLong();
 
-//        Country country = new Country("", "Ummetopia", 22);
-//        countryDAO.update("UME", country);
+                    Country countryToCreate = new Country(countryCode, countryName, countryPopulation);
 
-//        countryDAO.delete("GRG");
+                    countryDAO.create(countryToCreate);
 
+                    break;
+                case 4:
+                    System.out.print("Please provide the country code to update: ");
+                    String countryCodeToUpdate = scanner.nextLine();
+
+                    System.out.println("Please provide the information to update");
+                    System.out.print("Name: ");
+                    String countryNameToUpdate = scanner.nextLine();
+                    System.out.print("Population: ");
+                    long countryPopulationToUpdate = scanner.nextLong();
+
+                    Country countryToUpdate = new Country("", countryNameToUpdate, countryPopulationToUpdate);
+
+                    countryDAO.update(countryCodeToUpdate, countryToUpdate);
+                    break;
+                case 5:
+                    System.out.print("Please provide the country code to delete: ");
+                    String countryCodeToDelete = scanner.nextLine();
+
+                    countryDAO.delete(countryCodeToDelete);
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                default:
+                    System.out.println("Not valid command, try again");
+            }
+        } while(mainCommand != 0);
 
     }
 }
