ea88b71b7c63486e83808ec558fc27da066d2e43
Greg <no@no.com>
2025-06-13 13:43:52 -0400

    added spring boot app

---
+++ workbooks/workbook-9/working_with_spring_boot/src/main/java/com/ps/working_with_spring_boot/WorkingWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ps.working_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingWithSpringBootApplication.class, args);
+	}
+
+}
