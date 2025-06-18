b09b408f2037417b7b0e0b23a61e8418563e0c18
Greg <no@no.com>
2025-06-18 08:58:34 -0400

    created new REST project for product

---
+++ workbooks/workbook-9/workingWith_product/src/main/java/com/ps/workingWith_product/WorkingWithProductApplication.java
@@ -0,0 +1,30 @@
+package com.ps.workingWith_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingWithProductApplication.class, args);
+
+		// Download spring app from initialr
+		// Make sure our dependencies are setup correctly
+		// Create necessary packages
+		// Setup database, provided db settings to application.properties
+		// Create the config file to gain access to the data source
+
+		// Creating our product controller file
+		// Add a single endpoint GET /products in the controller file
+
+		// Create a DAO for product interface/implementation
+		// Add the getAll method the DAO
+
+		// Test it/See all products from northwind
+
+		// Add the rest of the CRUD endpoints
+
+	}
+
+}
---
+++ workbooks/workbook-9/workingWith_product/src/test/java/com/ps/workingWith_product/WorkingWithProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.workingWith_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingWithProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
