666da6c1b7add6015d197f1c20db1b23218815bf
Greg <no@no.com>
2025-06-18 11:08:09 -0400

    added product model

--- workbooks/workbook-9/workingWith_product/src/main/java/com/ps/workingWith_product/controllers/ProductController.java
+++ workbooks/workbook-9/workingWith_product/src/main/java/com/ps/workingWith_product/controllers/ProductController.java
@@ -1,4 +1,14 @@
 package com.ps.workingWith_product.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+
+
+    @GetMapping("/products")
+    public String getAllProducts(){
+        return "Test";
+    }
 }
---
+++ workbooks/workbook-9/workingWith_product/src/main/java/com/ps/workingWith_product/models/Product.java
@@ -0,0 +1,48 @@
+package com.ps.workingWith_product.models;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private double unitPrice;
+
+    public Product() {}
+
+    public Product(int productId, String productName, double unitPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
